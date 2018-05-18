eaf0d005a78a86f3694273c9cf7594074abdccca
Edxael <zerphyr.r@gmail.com>
2018-05-18 01:00:19 -0600

    update

--- src/Z-General.test.js
+++ src/Z-General.test.js
@@ -1,15 +1,74 @@
+// ===[ Gral-Imports ]================================
 import React from 'react'
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import sinon from 'sinon'
 
 
-// ===[ Test SUM ]================================
 
-import HomeComponent from './MyApp/01-Home'
 
-Enzyme.configure({ adapter: new Adapter() })
-const app = Enzyme.shallow(<HomeComponent />)
- 
-it('renders correctly', () => {
-  expect(app).toMatchSnapshot()
+// ***[ Links ]***********************************************************
+// https://airbnb.io/projects/enzyme/
+
+// Locations:
+// https://jonbellah.com/articles/intro-react-testing/
+// https://github.com/airbnb/enzyme/issues/404
+// https://github.com/airbnb/enzyme/issues/323
+
+
+
+
+// ===[ Test SUM ]====================================
+import SumComp from './MyApp/02-Sum'
+
+describe('<SumComp />', () => { 
+  Enzyme.configure({ adapter: new Adapter() })
+  
+  it('renders correctly', () => {
+    const Wrapper = Enzyme.shallow(<SumComp />)
+    expect.anything(Wrapper)
+  })
+
+
+  it('should render children when passed in', () => {
+    const wrapper = Enzyme.shallow(
+      <SumComp>
+        <div className="SumCont" />
+      </SumComp>     );
+    expect(wrapper.contains(<div className="SumCont" />)).to.be.true;
+  });
+
+
+  // it('simulates click events', () => {
+  //   const mockOnClick = sinon.spy();
+  //   const wrapper = Enzyme.shallow(
+  //     <SumComp>
+  //       <button onClick={ mockOnClick } >Add Numbers</button>
+  //     </SumComp>    
+  //   )
+  //   wrapper.find('button').simulate('click');
+  //   expect(mockOnClick.calledOnce).to.be.true;
+  // });
+
 })
+// ---------------------------------------------------
+
+
+
+
+// ===[ Test XYZ ]====================================
+
+
+
+// it('simulates click events', () => {
+//   const mockOnClick = sinon.spy();
+//     const wrapper = Enzyme.shallow(<SumComp onClick={mockOnClick}/>);
+//     wrapper.find('button').simulate('click');
+//     expect(mockOnClick.calledOnce).to.be.true;
+// })
+
+
+// expect(onButtonClick.calledOnce).to.equal(true);
+
+
+
---
+++ zinfo.md
@@ -0,0 +1,17 @@
+# Documentation while in Dev.
+
+URL: 
+https://facebook.github.io/jest/docs/en/mock-function-api.html
+
+
+
+=============================================================
+### Videos:
+
+How to Test React Components [19:10]
+https://www.youtube.com/watch?v=f6Uk0qS_Lho
+
+
+Traversy-Media: Jest Crash Course - Unit testing in JS [57:18]
+https://www.youtube.com/watch?v=7r4xVDI2vho
+
