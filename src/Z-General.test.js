91caea485b4999cb555200faf3bfc5099c042ff2
Edxael <zerphyr.r@gmail.com>
2018-05-20 13:50:09 -0600

    update

--- src/Z-General.test.js
+++ src/Z-General.test.js
@@ -1,8 +1,6 @@
 // ===[ Gral-Imports ]================================
 import React from 'react'
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import sinon from 'sinon'
+
 
 
 
@@ -19,56 +17,3 @@ import sinon from 'sinon'
 
 
 // ===[ Test SUM ]====================================
-import SumComp from './MyApp/02-Sum'
-
-describe('<SumComp />', () => { 
-  Enzyme.configure({ adapter: new Adapter() })
-  
-  it('renders correctly', () => {
-    const Wrapper = Enzyme.shallow(<SumComp />)
-    expect.anything(Wrapper)
-  })
-
-
-  it('should render children when passed in', () => {
-    const wrapper = Enzyme.shallow(
-      <SumComp>
-        <div className="SumCont" />
-      </SumComp>     );
-    expect(wrapper.contains(<div className="SumCont" />)).to.be.true;
-  });
-
-
-  // it('simulates click events', () => {
-  //   const mockOnClick = sinon.spy();
-  //   const wrapper = Enzyme.shallow(
-  //     <SumComp>
-  //       <button onClick={ mockOnClick } >Add Numbers</button>
-  //     </SumComp>    
-  //   )
-  //   wrapper.find('button').simulate('click');
-  //   expect(mockOnClick.calledOnce).to.be.true;
-  // });
-
-})
-// ---------------------------------------------------
-
-
-
-
-// ===[ Test XYZ ]====================================
-
-
-
-// it('simulates click events', () => {
-//   const mockOnClick = sinon.spy();
-//     const wrapper = Enzyme.shallow(<SumComp onClick={mockOnClick}/>);
-//     wrapper.find('button').simulate('click');
-//     expect(mockOnClick.calledOnce).to.be.true;
-// })
-
-
-// expect(onButtonClick.calledOnce).to.equal(true);
-
-
-
